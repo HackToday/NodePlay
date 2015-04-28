b87d4fa3eb2865ba963b1e4266419f4b504ff200
Kennan <wkq5325@gmail.com>
2015-04-28 07:25:31 +0000

    Support more songs

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,18 +2,10 @@
 
 var musicApp = angular.module('musicApp', []);
 
-musicApp.controller('MusicListCtrl', function($scope) {
-  $scope.songs = [
-    { 'name' : 'See you again',
-      'time' : 1984,
-      'snippet': 'A lovely song for the lost'},
-    { 'name' : 'Hit now',
-      'time' : 1996,
-      'snippet': 'A hot song for the lazy people'},
-    { 'name' : 'Sunset',
-      'time' : 2009,
-      'snippet': 'A peaceful song for enjoy'}
-  ];
-
-  $scope.orderProp = 'time'
-});
+musicApp.controller('MusicListCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('songs/songs.json').success(function(data) {
+      $scope.songs = data;
+    });
+  $scope.orderProp = 'length'
+}]);

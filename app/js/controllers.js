619ca0fb548004d00a6861df57210f92c1cbdb0d
Kennan <wkq5325@gmail.com>
2015-04-28 02:46:25 +0000

    Add angular templates sample usage

---
+++ app/js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var musicApp = angular.module('musicApp', []);
+
+musicApp.controller('MusicListCtrl', function ($scope) {
+  $scope.songs = [
+    { 'name' : 'See you again',
+      'snippet': 'A lovely song for the lost'},
+    { 'name' : 'Hit now',
+      'snippet': 'A hot song for the lazy people'},
+    { 'name' : 'Sunset',
+      'snippet': 'A peaceful song for enjoy'}
+  ];
+});

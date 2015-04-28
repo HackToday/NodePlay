2758bb31547ee4a288be1cb9d76eb606eb75e686
Kennan <wkq5325@gmail.com>
2015-04-28 06:46:50 +0000

    Add two way data binding sample

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,10 +5,15 @@ var musicApp = angular.module('musicApp', []);
 musicApp.controller('MusicListCtrl', function($scope) {
   $scope.songs = [
     { 'name' : 'See you again',
+      'time' : 1984,
       'snippet': 'A lovely song for the lost'},
     { 'name' : 'Hit now',
+      'time' : 1996,
       'snippet': 'A hot song for the lazy people'},
     { 'name' : 'Sunset',
+      'time' : 2009,
       'snippet': 'A peaceful song for enjoy'}
   ];
+
+  $scope.orderProp = 'time'
 });
